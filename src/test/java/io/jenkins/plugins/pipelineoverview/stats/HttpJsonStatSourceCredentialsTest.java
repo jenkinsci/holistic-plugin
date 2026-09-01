437fb93c4b63438951abbb2b2ceb78389487ede3
Colere-Sys <anashassari@protonmail.com>
2026-09-01 17:31:14 +0200

    Test the credentials dropdown permission denial

--- src/test/java/io/jenkins/plugins/pipelineoverview/stats/HttpJsonStatSourceCredentialsTest.java
+++ src/test/java/io/jenkins/plugins/pipelineoverview/stats/HttpJsonStatSourceCredentialsTest.java
@@ -8,8 +8,14 @@ import com.sun.net.httpserver.HttpServer;
 import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
 import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.MockAuthorizationStrategy;
 import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
+import hudson.model.User;
+import hudson.security.ACL;
+import hudson.security.ACLContext;
+import hudson.util.ListBoxModel;
 import hudson.util.Secret;
+import jenkins.model.Jenkins;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -18,6 +24,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -112,5 +119,23 @@ class HttpJsonStatSourceCredentialsTest {
                 .getDescriptorByType(HttpJsonStatSource.DescriptorImpl.class);
         assertTrue(d.doFillCredentialsIdItems(null, "").stream()
                 .anyMatch(o -> "tok2".equals(o.value)));
+    }
+
+    @Test
+    void nonAdministratorDoesNotSeeTheCredentialListButKeepsCurrentValue(JenkinsRule j) throws Exception {
+        j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
+        j.jenkins.setAuthorizationStrategy(new MockAuthorizationStrategy()
+                .grant(Jenkins.READ).everywhere().to("alice"));
+        SystemCredentialsProvider.getInstance().getDomainCredentialsMap().get(Domain.global())
+                .add(new StringCredentialsImpl(CredentialsScope.GLOBAL, "withheld", "desc",
+                        Secret.fromString("s3cr3t")));
+        HttpJsonStatSource.DescriptorImpl d = j.jenkins
+                .getDescriptorByType(HttpJsonStatSource.DescriptorImpl.class);
+        ListBoxModel options;
+        try (ACLContext ctx = ACL.as(User.getById("alice", true))) {
+            options = d.doFillCredentialsIdItems(null, "previously-selected-id");
+        }
+        assertFalse(options.stream().anyMatch(o -> "withheld".equals(o.value)));
+        assertTrue(options.stream().anyMatch(o -> "previously-selected-id".equals(o.value)));
     }
 }
